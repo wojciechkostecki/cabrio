0c5b52ccef482e5e3f0aba4dba572ebfb79ef4a3
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-03-31 07:37:15 +0200

    Removing a parameter from the method, adding a field isCarMoving, shortening the method

--- src/Cabrio.java
+++ src/Cabrio.java
@@ -2,16 +2,14 @@ public class Cabrio {
     public static String model;
     public static String brand;
     public static boolean isRoofOpen;
+    public static boolean isCarMoving;
 
-    public static void toggleRoof(boolean isCarMoving) {
+    public static void toggleRoof() {
         if (isCarMoving) {
             System.out.println("Nie można otworzyć/zamknąć dachu");
-        } else if (isRoofOpen) {
-            isRoofOpen = false;
-            System.out.println("zamknięto dach");
         } else {
-            isRoofOpen = true;
-            System.out.println("otworzono dach");
+            isRoofOpen = !isRoofOpen;
+            System.out.println(isRoofOpen ? "otworzono" : "zamknięto");
         }
     }
 }
--- src/CarTest.java
+++ src/CarTest.java
@@ -2,8 +2,9 @@ public class CarTest {
     public static void main(String[] args) {
         Cabrio.brand = "BMW";
         Cabrio.model = "M850i";
-        Cabrio.isRoofOpen = true;
+        Cabrio.isRoofOpen = false;
+        Cabrio.isCarMoving = false;
 
-        Cabrio.toggleRoof(false);
+        Cabrio.toggleRoof();
     }
 }
